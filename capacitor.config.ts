45b72d5a1901cd9de18a3f1054f29204f5d1d9f3
KhderBador <165778345+KhderBador@users.noreply.github.com>
2025-06-22 13:06:37 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'home_sports_website',
+  webDir: 'build'
+};
+
+export default config;
